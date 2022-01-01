c986467cd66fe46c85b723de2aec7af7181c4894
Shrijit Singh <shrijitsingh99@gmail.com>
2021-12-31 23:35:23 -0500

    Add support for navigation

---
+++ launch/navigation.launch.py
@@ -0,0 +1,127 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node, SetRemap
+from launch_ros.substitutions import FindPackageShare
+from nav2_common.launch import RewrittenYaml
+
+
+def generate_launch_description():
+    current_pkg = FindPackageShare('spraybot_bringup')
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    autostart = LaunchConfiguration('autostart')
+    map_yaml_file = LaunchConfiguration('map')
+
+    lifecycle_nodes_navigation = ['controller_server',
+                                  'planner_server',
+                                  'recoveries_server',
+                                  'bt_navigator',
+                                  'waypoint_follower']
+
+    lifecycle_nodes_localization = ['map_server']
+
+    # Create temporary YAML files that include substitutions
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'autostart': autostart,
+        'yaml_filename': map_yaml_file}
+
+    configured_params = RewrittenYaml(
+            source_file=LaunchConfiguration('params_file'),
+            root_key='',
+            param_rewrites=param_substitutions,
+            convert_types=True)
+
+    return LaunchDescription([
+        # Map fully qualified names to relative ones so the node's namespace can be prepended.
+        SetRemap(src=['/tf', '/tf_static'], dst=['tf', 'tf_static']),
+
+        # Set env var to print messages to stdout immediately
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+
+        DeclareLaunchArgument(
+            'use_sim_time', default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        DeclareLaunchArgument(
+            'autostart', default_value='true',
+            description='Automatically startup the nav2 stack'),
+
+        DeclareLaunchArgument(
+            'map',
+            default_value=PathJoinSubstitution([current_pkg, 'maps', 'empty.yaml']),
+            description='Full path to map yaml file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=PathJoinSubstitution([current_pkg, 'params', 'navigation.yaml']),
+            description='Full path to the ROS2 parameters file to use'),
+
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            output='screen',
+            parameters=[configured_params]),
+
+        # Node(
+        #     package='nav2_smoother',
+        #     executable='smoother_server',
+        #     name='smoother_server',
+        #     output='screen',
+        #     parameters=[configured_params]),
+
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[configured_params]),
+
+        Node(
+            package='nav2_recoveries',
+            executable='recoveries_server',
+            name='recoveries_server',
+            output='screen',
+            parameters=[configured_params]),
+
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[configured_params]),
+
+        Node(
+            package='nav2_waypoint_follower',
+            executable='waypoint_follower',
+            name='waypoint_follower',
+            output='screen',
+            parameters=[configured_params]),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_navigation',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes_navigation}]),
+
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[configured_params]),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes_localization}])
+
+    ])
