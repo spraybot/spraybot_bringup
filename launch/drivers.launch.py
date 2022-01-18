6025f62b3057b8206b4d8850e253dab727fa1125
Shrijit Singh <shrijitsingh99@gmail.com>
2022-01-18 18:40:34 -0500

    Add support for managing sensors using nav2 lifecycle manager

--- launch/drivers.launch.py
+++ launch/drivers.launch.py
@@ -3,7 +3,9 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
@@ -13,12 +15,56 @@ def generate_launch_description():
     vectornav_pkg = FindPackageShare('vectornav')
     # ouster_pkg = FindPackageShare('ros2_ouster')
 
+    lifecycle_nodes_sensors = ['vectornav']
+
     # Declare the launch arguments
     declare_enable_visualization = DeclareLaunchArgument(
         'sensors',
         default_value='true',
         description='Start VectorNav and Ouster nodes')
     ld.add_action(declare_enable_visualization)
+
+    vectornav_params = RewrittenYaml(
+            source_file=PathJoinSubstitution([vectornav_pkg, 'config', 'vectornav.yaml']),
+            root_key='',
+            param_rewrites={'autostart': 'false'},
+            convert_types=True)
+
+    vectornav_node = Node(
+        package='vectornav',
+        executable='vectornav',
+        output='screen',
+        parameters=[vectornav_params])
+    ld.add_action(vectornav_node)
+
+    vectornav_sensor_msgs_node = Node(
+        package='vectornav',
+        executable='vn_sensor_msgs',
+        output='screen',
+        parameters=[vectornav_params],
+        remappings=[('vectornav/imu', 'imu/data'),
+                    ('vectornav/gnss/ins', 'gps/fix'),
+                    ('vectornav/gnss/raw', 'gps/fix/raw')])
+    ld.add_action(vectornav_sensor_msgs_node)
+
+    # TODO: Create custom lifecycle manger wit bond and auto-restart support
+    lifecycle_manager_sensors = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_sensors',
+        output='screen',
+        parameters=[{'autostart': True},
+                    {'node_names': lifecycle_nodes_sensors,
+                    'bond_timeout': 0.0}],
+        condition=IfCondition(LaunchConfiguration('sensors')))
+    ld.add_action(lifecycle_manager_sensors)
+
+    # To be added after integrating Ouster hardware
+    # ouster_launch = IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource(
+    #             PathJoinSubstitution([ouster_pkg, 'launch', 'driver_launch.py'])),
+    #         condition=IfCondition(LaunchConfiguration('sensors')))
+    # ld.add_action(ouster_launch)
 
     # Include other launch files
     base_launch = IncludeLaunchDescription(
@@ -30,18 +76,5 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             PathJoinSubstitution([husky_control_pkg, 'launch', 'teleop_launch.py'])))
     ld.add_action(teleop_launch)
-
-    vectornav_launch = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                PathJoinSubstitution([vectornav_pkg, 'launch', 'vectornav.launch.py'])),
-            condition=IfCondition(LaunchConfiguration('sensors')))
-    ld.add_action(vectornav_launch)
-
-    # To be added after integrating Ouster hardware
-    # ouster_launch = IncludeLaunchDescription(
-    #         PythonLaunchDescriptionSource(
-    #             PathJoinSubstitution([ouster_pkg, 'launch', 'driver_launch.py'])),
-    #         condition=IfCondition(LaunchConfiguration('sensors')))
-    # ld.add_action(ouster_launch)
 
     return ld
