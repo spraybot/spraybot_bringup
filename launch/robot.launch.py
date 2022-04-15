25e4f7450db26b3ff84331981aab5ce3f2211f8e
Shrijit Singh <shrijitsingh99@gmail.com>
2022-04-15 12:33:12 -0400

    Disable driver launch, since its part of systemd

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -4,6 +4,7 @@ from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -13,7 +14,7 @@ def generate_launch_description():
     # Declare the launch arguments
     declare_enable_drivers = DeclareLaunchArgument(
         'drivers',
-        default_value='true',
+        default_value='false',
         description='Start Husky, VectorNav and Ouster nodes')
     ld.add_action(declare_enable_drivers)
 
@@ -40,5 +41,17 @@ def generate_launch_description():
                 PathJoinSubstitution([current_pkg, 'launch', 'visualization.launch.py'])),
             condition=IfCondition(LaunchConfiguration('viz')))
     ld.add_action(visualization_launch)
+
+    # Waypoint teleop
+    node_waypoint_teleop = Node(
+        package='spraybot_utils',
+        executable='waypoint_teleop.py',
+        output='screen',
+        parameters=[{
+            'waypoint_topic': '/goal_pose',
+            'axis_scaling_factor': 0.25
+        }],
+    )
+    ld.add_action(node_waypoint_teleop)
 
     return ld
